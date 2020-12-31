84d5abd8391ccf6d37ba0865f0e481318863d740
nikitha26 <mnikitha365@gmail.com>
2020-12-31 15:59:01 +0530

    save data and get data

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-//This function is work on state change.ex:loged in , if condition is exected loged out ,else condition is exectuted. 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      document.getElementById("userdiv").style.display="block"
-      document.getElementById("logindiv").style.display="none"
+var userdata = document.querySelector('#cafe-list')
+const form  = document.querySelector('#add-cafe-form')
+//create element and reder data
+function render(doc){
+   let li = document.createElement('li');
+   let username = document.createElement('span')
+   let favcolor = document.createElement('span')
 
-    } else {
-        document.getElementById("userdiv").style.display="none"
-        document.getElementById("logindiv").style.display="block"
-    }
-  });
+   li.setAttribute('data-id',doc.id);
+   username.textContent = doc.data().username;
+   favcolor.textContent = doc.data().favcolor;
+    
+   li.appendChild(username)
+   li.appendChild(favcolor);
 
-
-function login(){
-    var email = document.getElementById("email field").value;
-    var psw = document.getElementById("psw field").value;
-      alert(email)
-    firebase.auth().signInWithEmailAndPassword(email, psw)
-    .then((user) => {
-      
-   })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    alert("Error:"+errorMessage)
-  });
+   userdata.appendChild(li);
 }
+
+//get data from firebase
+db.collection('registration').get().then((snapshot) =>{
+  snapshot.docs.forEach(doc => {
+      render(doc)
+  });
+})
+
+//saving data
+form.addEventListener('submit',(e) =>{
+  e.preventDefault()
+  db.collection('registration').add({
+    username:form.name.value,
+    favcolor:form.color.value,
+  })
+  form.name.value = '';
+  form.color.value = '';
+})
